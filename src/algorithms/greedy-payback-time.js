4debf01176bd09b333e7f9c99c22611d9e9da13b
Lauritz <lblohm25@student.aau.dk>
2026-03-30 15:20:03 +0200

    added untested greedy payback time logic

--- src/algorithms/greedy-payback-time.js
+++ src/algorithms/greedy-payback-time.js
@@ -1,12 +1,13 @@
+import { loadBuildings } from "../cookie-clicker/purchasables/building.js";
 import Algorithm from "./algorithm.js";
 import Decision from "./decision.js";
 
-export default class GreedyTime extends Algorithm {
+export default class GreedyPaybackTime extends Algorithm {
 	// Dummy to automatically add an instance of the algorithm to the derived set in the Algorithm class.
 	static dummy = Algorithm.derived.add({
 		name: this.name,
 		title: "[Greedy] Save+Payback",
-		instance: new GreedyTime(),
+		instance: new GreedyPaybackTime(),
 	});
 
 	/**
@@ -16,6 +17,28 @@ export default class GreedyTime extends Algorithm {
 	 */
 	getNextDecision(gameState, buildings) {
 		// implementer "smart" payback greedy algoritme, erstat "buildings["cursor"]" herunder
-		return new Decision(gameState, buildings["cursor"]);
+
+		let bestDecision = [buildings["cursor"], 0];
+		let tempDecision = [buildings["cursor"], 0];
+		let saveUpTime = 0;
+		let paybackTime = 0;
+		let paybackSaveUpTime = 0;
+		console.log(buildings);
+
+		for(let key in buildings){
+
+			paybackTime = buildings[key].cost/buildings[key].baseCpS;
+			saveUpTime = buildings[key].cost/gameState.cps;
+			paybackSaveUpTime = saveUpTime + paybackTime;
+			tempDecision = [buildings[key], paybackSaveUpTime];
+
+			if(tempDecision[1] >= bestDecision[1]){
+				bestDecision[0] = tempDecision[0];
+				bestDecision[1] = tempDecision[1];
+			}
+		}
+		console.log(bestDecision[0]);
+
+		return new Decision(gameState, bestDecision[0]);
 	}
 }
