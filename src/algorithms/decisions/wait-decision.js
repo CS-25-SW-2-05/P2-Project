c81cfe19b144e6ac3de10fbef0c6eebd836d7b85
Bugge <fbj070106@gmail.com>
2026-04-20 23:31:46 +0200

    Table Decision Visualization

--- src/algorithms/decisions/wait-decision.js
+++ src/algorithms/decisions/wait-decision.js
@@ -1,24 +1,26 @@
 import Decision from "./decision.js";
 
 export default class WaitDecision extends Decision {
-	/**
-	 * @param {GameState} gameState
-	 * @param {number} wait
-	 */
-	constructor(gameState, wait) {
-		super(gameState);
+    /**
+     * @param {GameState} gameState
+     * @param {number} wait
+     */
+    constructor(gameState, wait) {
+        super(gameState);
 
-		this._wait = wait;
-		this.isValid = this._wait > 0;
-	}
+        this.wait = wait;
+        this.isValid = this.wait > 0;
+    }
 
-	perform() {
-		this.beforeCookies = this._gameState.cookies;
-		this._gameState.simulationTime += this._wait;
-		this._gameState.cookies += this._gameState.cps * this._wait;
-		this.afterCookies = this._gameState.cookies;
+    perform() {
+        super.perform();
+        this.cookiesBefore = this._gameState.cookies;
+        this._gameState.simulationTime += this.wait;
+        this._gameState.cookies += this._gameState.cps * this.wait;
+        this.cookiesAfter = this._gameState.cookies;
+        this.cpsAfter = this._gameState.buildingCpS;
 
-		console.log("Result:", this._gameState);
-		return true;
-	}
+        console.log("Result:", this._gameState);
+        return true;
+    }
 }
